1478620a9b9d77b73c203bc97a588315da3a90f6
senaKash <ku.kuptsov@yandex.ru>
2024-04-18 15:17:46 +0300

    отредактирована структура приложения

---
+++ src/Board.Contracts/Attributes/NameValidationAttributes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board.Contracts.Attributes
+{
+    public class NameValidationAttribute : ValidationAttribute
+    {
+        
+        /*
+        public override bool IsValid(object? value)
+        {
+
+            string? valueAsString = (string)value;
+            return !valueAsString.Contains("хаймарс");
+        }
+        */
+
+        
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string? valueAsString = (string)value;
+
+            var service = (IForbiddenWordsService)validationContext.GetService(typeof(IForbiddenWordsService));
+
+            var forbiddenWords = service.GetForbiddenWords();
+
+            return forbiddenWords.Contains(valueAsString)
+            ? new ValidationResult("Наименование содержит запрещенные слова")
+            : ValidationResult.Success;
+        }
+        
+
+    }
+}
